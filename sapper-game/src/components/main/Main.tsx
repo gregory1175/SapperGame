b7ef2f9470b7a779bc03e38933eea3428b1b65f7
gregory1175 <151869985+gregory1175@users.noreply.github.com>
2024-06-25 11:46:48 +0300

    BaseVer

---
+++ sapper-game/src/components/main/Main.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import App from "../app/App"
+import style from './Main.module.scss'
+export const Main = () => {
+	return (
+        <>
+        <header>
+            Добро пожаловать
+        </header>
+        <App />
+
+        <footer>
+        </footer>
+        </>
+	);
+};
---
+++ sapper-game/src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/index.scss'
+import { Main } from './components/main/Main'
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+)
+
+
